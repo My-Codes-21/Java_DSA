f84afc52ccdfb2e8d12ced3fd4efb20bad208157
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-09-05 16:48:39 +0530

    first commit

---
+++ src/DAY_1/Class1.java
@@ -0,0 +1,24 @@
+package DAY_1;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Class1 {
+    public static void main(String[] args){
+        //printing z using \n
+        System.out.println("*****\n   *\n  *\n *\n*\n*****");
+        int a=20;
+        int b = 15;
+        int sum = a+b;
+        int pro =a*b;
+        int sub =a-b;
+        int divi =a/b;
+        int mod = a%b;
+        int exp = (a*b)/(a+b);
+        System.out.println(sub);
+        System.out.println(sum);
+        System.out.println(pro);
+        System.out.println(divi);
+        System.out.println(exp);
+        System.out.println(mod);
+    }
+}

6686b34fa6214ade51684f8ca681506cad271b47
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-09-05 17:56:39 +0530

    first commit

--- src/DAY_1/Class1.java
+++ src/DAY_1/Class1.java
@@ -1,13 +1,15 @@
 package DAY_1;
 
-import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
 
 public class Class1 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
         //printing z using \n
         System.out.println("*****\n   *\n  *\n *\n*\n*****");
-        int a=20;
-        int b = 15;
+        int a=15;
+        int b = 20;
         int sum = a+b;
         int pro =a*b;
         int sub =a-b;
@@ -28,5 +30,39 @@ public class Class1 {
             System.out.println("The number is odd");
         }
         System.out.println("Condition exited");
+
+        if(a ==b ){
+            System.out.println("a is equal to b");
+
+        }
+        else {
+            if (a > b){
+                System.out.println("a is greater than b");
+
+            }
+            else {
+                System.out.println("a is smaller than b");
+            }
+        }
+//         marks condition checker
+        int marks=sc.nextInt();
+        if (marks > 90){
+            System.out.println("Excellent");
+        }
+        else if (marks >80 && marks <= 90){
+            System.out.println("Good");
+
+        }else {
+            if (marks >70 && marks <=80){
+                System.out.println("Fair");
+            }
+            else if (marks >= 60 && marks <=70){
+                System.out.println("Meets Expectation");
+
+            }
+            else {
+                System.out.println("below par");
+            }
+        }
     }
 }

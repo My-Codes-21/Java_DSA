29a61497b096f6fef764d0a2b686b9c850cee19c
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-09-13 17:41:44 +0530

    day 2 low to high prime numbers

---
+++ src/DAY_2/LowToHigh.java
@@ -0,0 +1,22 @@
+package DAY_2;
+import java.util.Scanner;
+public class LowToHigh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the lowest number : ");
+        int low = sc.nextInt();
+        System.out.print("Enter the highest number : ");
+        int high = sc.nextInt();
+        for (int i=low;i<=high;i++){
+            int count =0;
+            for (int j=1;j<=i;j++){
+                if (i%j==0){
+                    count++;
+                }
+            }
+            if (count==2){
+                System.out.println(i);
+            }
+        }
+    }
+}

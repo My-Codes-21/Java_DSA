fe060be80c148fb3bb753ed8598ed250d8f14e77
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-09-17 21:57:15 +0530

    printing the star pattern with input

---
+++ src/DAY_5/Pattern_1.java
@@ -0,0 +1,20 @@
+package DAY_5;
+import java.util.*;
+public class Pattern_1 {
+    public static void main(String[] args){
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){    // this loop is just increasing the value
+
+            for(int j = 1; j <= i; j++){   //this loop is printing the stars in sameline
+
+                System.out.print(" * ");
+
+            }
+              System.out.println(""); // this is only for new line
+
+        }
+    }
+}

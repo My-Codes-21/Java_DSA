25cbb1c0197f3fb0a69b60d04ef5f55dbf9d84a7
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-09-20 16:48:24 +0530

    Day 8 codes executed

---
+++ src/DAY_8/Pattern_9.java
@@ -0,0 +1,20 @@
+package DAY_8;
+import java.util.*;
+public class Pattern_9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j || i+j==n+1){
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
